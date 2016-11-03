7f1a033023cfc38a6f49402872702b640fc91c62
Rob <rob@rob-VirtualBox.(none)>
2016-11-03 11:20:51 -0400

    update test-go

--- exercises/test-go.go
+++ exercises/test-go.go
@@ -2,14 +2,30 @@ package main
 
 import "fmt"
 
+import . "github.com/tj/go-debug"
+import "time"
+
+var a = Debug("goroutine:a")
+var b = Debug("goroutine:b")
+var c = Debug("goroutine:c")
+
+func work(debug DebugFunction, delay time.Duration) {
+	for {
+		fmt.Println("test3")
+		debug("doing stuff")
+		time.Sleep(delay)
+	}
+}
+
 func main() {
 
 	defer fmt.Println("this is the last line")
-	numSlice := [] int {5,4,3,2,1}
-
-        fmt.Println(numSlice[2])
+	//q := make(chan bool)
+	go work(a, 1000*time.Millisecond)
+	go work(b, 250*time.Millisecond)
+	go work(c, 100*time.Millisecond)
+	fmt.Println("test4")
+	time.Sleep(10*time.Second)
+	//<-q
 
 }
-
-
-
